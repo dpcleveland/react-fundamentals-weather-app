7d9a6fd5175519ff1b38dda2352c7a7a7464daeb
Daniel Cleveland <dpcleveland@gmail.com>
2017-01-20 18:34:05 -0600

    Setup handlers for city form submission

--- app/components/GetCity.js
+++ app/components/GetCity.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
 var styles = {
   cityInputContainer: {
@@ -9,16 +10,29 @@ var styles = {
   },
   cityButton: {
 
+  },
+  horizontal: {
+
+  },
+  vertical: {
+
   }
 };
 
 function GetCity (props) {
   return (
-    <div style={styles.cityInputContainer}>
-      <input style={styles.cityInput} />
-      <button style={styles.cityButton}></button>
+    <div style={styles.cityInputContainer} className={props.direction}>
+      <form onSubmit={props.onSubmitCity}>
+        <input style={styles.cityInput} onChange={props.onUpdateCity} placeholder="City, State" type="text" value={props.city} />
+        <button style={styles.cityButton} type="submit">Get Weather</button>
+      </form>
     </div>
   )
+}
+
+GetCity.propTypes = {
+  onSubmitCity: PropTypes.func.isRequired,
+  onUpdateCity: PropTypes.func.isRequired,
 }
 
 module.exports = GetCity;
--- app/containers/GetCityContainer.js
+++ app/containers/GetCityContainer.js
@@ -2,9 +2,37 @@ var React = require('react');
 var GetCity = require('../components/GetCity');
 
 var GetCityContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      direction: 'horizontal',
+      city: ''
+    }
+  },
+  handleSubmitCity: function (event) {
+    event.preventDefault();
+    var city = this.state.city;
+    this.setState({
+      city: ''
+    });
+
+    this.context.router.push('/forecast/' + this.state.city);
+
+  },
+  handleUpdateCity: function (event) {
+    this.setState({
+      city: event.target.value
+    });
+  },
   render: function () {
     return (
-      <GetCity />
+      <GetCity
+        onSubmitCity={this.handleSubmitCity}
+        onUpdateCity={this.handleUpdateCity}
+        city={this.state.city}
+        direction={this.state.direction} />
     )
 
   }
